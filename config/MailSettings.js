b9a67ec0f7d0d3c002b67f844cdb4f312b14ae5f
Sangram Desai <sangram.d@westagilelabs.com>
2021-04-15 16:12:30 +0530

    added api documentation

--- config/MailSettings.js
+++ config/MailSettings.js
@@ -4,6 +4,7 @@ module.exports = {
     "SMTP_PORT":25,
     "SMTP_USERNAME":"62bba1ea70e85f",
     "SMTP_PASSWORD":"c14bed28b97023",
+
     "APITOKEN":"4c5dc1f9d694985787dd1461c9d88be6",
     
     "POP3_HOST":"pop3.mailtrap.io",
